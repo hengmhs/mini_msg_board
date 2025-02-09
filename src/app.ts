6d74060f6912a8d290ad78890868c7ec51a7c953
hengmhs <hengmhs@gmail.com>
2025-02-09 16:58:49 +0800

    Add new message functionality

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,14 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 
+/*
+In order to get and use the data from your form, you will need to access the contents of your form inside router.post() as an object called req.body. 
+The individual fields inside the body object are named according to the name attribute on your inputs (the value of <input name="messageText"> will show up as req.body.messageText inside the router.post function). 
+For this to work as intended, you’ll need to use a app level Express middleware called express.urlencoded() to parse the form data into req.body
+*/
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", indexRouter);
 
 // Catch every thrown error
--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -7,4 +7,14 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+  const { user, text } = req.body;
+  messages.push({ text, user, added: new Date() });
+  res.redirect("/");
+});
+
 export default indexRouter;
