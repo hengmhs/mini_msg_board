19b3608e2cf1dada616f963c0928f576f9da24fc
hengmhs <hengmhs@gmail.com>
2025-02-09 17:04:18 +0800

    Move functions to controller

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -5,4 +5,14 @@ const getIndexPage = (req: Request, res: Response) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 };
 
-export { getIndexPage };
+const getNewMessageFormPage = (req: Request, res: Response) => {
+  res.render("form");
+};
+
+const postNewMessage = (req: Request, res: Response) => {
+  const { user, text } = req.body;
+  messages.push({ text, user, added: new Date() });
+  res.redirect("/");
+};
+
+export { getIndexPage, getNewMessageFormPage, postNewMessage };
--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -1,19 +1,17 @@
 import { Router } from "express";
 import { messages } from "../db";
-import { getIndexPage } from "../controllers/indexController";
+import {
+  getIndexPage,
+  getNewMessageFormPage,
+  postNewMessage,
+} from "../controllers/indexController";
 
 const indexRouter = Router();
 
 indexRouter.get("/", getIndexPage);
 
-indexRouter.get("/new", (req, res) => {
-  res.render("form");
-});
+indexRouter.get("/new", getNewMessageFormPage);
 
-indexRouter.post("/new", (req, res) => {
-  const { user, text } = req.body;
-  messages.push({ text, user, added: new Date() });
-  res.redirect("/");
-});
+indexRouter.post("/new", postNewMessage);
 
 export default indexRouter;
