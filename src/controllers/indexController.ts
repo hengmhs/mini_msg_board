7808c5ef2316eae19c4040d331f058d71d71002a
hengmhs <hengmhs@gmail.com>
2025-02-10 18:12:55 +0800

    Add express async error handler

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from "express";
 import { messages } from "../db";
+import expressAsyncHandler from "express-async-handler";
 
-const getIndexPage = (req: Request, res: Response) => {
+const getIndexPage = expressAsyncHandler((req: Request, res: Response) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
-};
+});
 
-const getNewMessageFormPage = (req: Request, res: Response) => {
-  res.render("form");
-};
+const getNewMessageFormPage = expressAsyncHandler(
+  (req: Request, res: Response) => {
+    res.render("form");
+  }
+);
 
-const postNewMessage = (req: Request, res: Response) => {
+const postNewMessage = expressAsyncHandler((req: Request, res: Response) => {
   const { user, text } = req.body;
   messages.push({ text, user, added: new Date() });
   res.redirect("/");
-};
+});
 
 export { getIndexPage, getNewMessageFormPage, postNewMessage };
---
+++ todo.md
@@ -0,0 +1,5 @@
+Add error handling library, express-async-handler
+
+Add an “open” button or link next to every message to open a new page with the message details.
+
+Push your project to GitHub.
