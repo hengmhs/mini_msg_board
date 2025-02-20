4849da0269162b8165660e66c6fb112744a66fda
hengmhs <hengmhs@gmail.com>
2025-02-20 20:42:27 +0800

    Implement add message functionality

--- src/controllers/messageDisplayController.ts
+++ src/controllers/messageDisplayController.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
-import { messages } from "../db";
 import expressAsyncHandler from "express-async-handler";
+import { getSpecificMessage } from "../db/queries";
 
-const getMessagePage = expressAsyncHandler((req: Request, res: Response) => {
-  const { id } = req.params;
+const getMessagePage = expressAsyncHandler(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  res.render("message", { message: messages[Number(id)] });
-});
+    const message = await getSpecificMessage(Number(id));
+
+    console.log(message);
+
+    res.render("message", { message: message[0] });
+  }
+);
 
 export { getMessagePage };
--- src/db/queries.ts
+++ src/db/queries.ts
@@ -2,5 +2,11 @@ import pool from "./pool";
 
 export const getAllMessages = async () => {
   const { rows } = await pool.query("SELECT * FROM messages");
+  console.log("getAllMessages: ", rows);
+  return rows;
+};
+
+export const getSpecificMessage = async (id: number) => {
+  const { rows } = await pool.query("SELECT * FROM messages WHERE id=$1", [id]);
   return rows;
 };
